b69598d77c0dfc4d00b31e9e7e2b7e0589a0022c
Eduardo Izquierdo <sirmartin@gmail.com>
2023-03-09 12:33:36 +0100

    Added game constants.

---
+++ GameConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeGame
+{
+    internal class GameConstants
+    {
+        public const int SNAKE_SIZE = 10;
+
+        public const int FRUIT_LIFESPAN = 100;
+
+        public const int SNAKE_START_POSITION_X = 200;
+
+        public const int SNAKE_START_POSITION_Y = 200;
+
+
+        /// <summary>
+        /// Initial Resolution
+        /// </summary>
+        public const int WINDOW_WIDTH = 500;
+        public const int WINDOW_HEIGHT = 500;
+    }
+}
