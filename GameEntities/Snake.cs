2b29bb985b85506df034c5d9a6a7a6916de11b48
Eduardo Izquierdo <sirmartin@gmail.com>
2023-03-09 12:34:37 +0100

    Started with some logic.
    Game bounderies game over conditions, and draw snake from own entity.

---
+++ GameEntities/Snake.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SnakeGame.Entities;
+using SnakeGame.Enums;
+using System;
+
+
+namespace SnakeGame.GameEntities
+{
+    public class Snake
+    {
+        public int Length { get; set; }
+
+        public MoveTypes Direction { get; set; }
+
+        public Coordinates Position { get; set; }
+
+        public ContentManager Content
+        {
+            get { return _content; }
+        }
+        ContentManager _content;
+
+        public Snake(IServiceProvider serviceProvider) {
+            Position = new Coordinates(GameConstants.SNAKE_START_POSITION_X, GameConstants.SNAKE_START_POSITION_Y);
+            Direction = MoveTypes.None;
+            _content = new ContentManager(serviceProvider, "Content");
+        }
+
+        public void Update(KeyboardState keyboard)
+        {
+            UpdateDirection(keyboard);
+
+            Move();
+        }
+
+        public bool IsAlive()
+        {
+            int MaxSize = 800;
+            if (Position.X < 0 || Position.Y < 0 || Position.X > MaxSize || Position.Y > MaxSize)
+                return false;
+
+            return true;
+        }
+
+        private void UpdateDirection(KeyboardState keyboard)
+        {
+            if (keyboard.IsKeyDown(Keys.Up))
+            {
+                Direction = MoveTypes.Up;
+            }
+            else if (keyboard.IsKeyDown(Keys.Down))
+            {
+                Direction = MoveTypes.Down;
+            }
+            else if (keyboard.IsKeyDown(Keys.Left))
+            {
+                Direction = MoveTypes.Left;
+            }
+            else if (keyboard.IsKeyDown(Keys.Right))
+            {
+                Direction = MoveTypes.Right;
+            }
+        }
+
+        private void Move()
+        {
+            switch (Direction)
+            {
+                case MoveTypes.None:
+                    break;
+                case MoveTypes.Up:
+                    Position.Y = Position.Y - GameConstants.SNAKE_SIZE;
+                    break;
+                case MoveTypes.Right:
+                    Position.X = Position.X + GameConstants.SNAKE_SIZE;
+                    break;
+                case MoveTypes.Down:
+                    Position.Y = Position.Y + GameConstants.SNAKE_SIZE;
+                    break;
+                case MoveTypes.Left:
+                    Position.X = Position.X - GameConstants.SNAKE_SIZE;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Draw(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch)
+        {
+            if (!IsAlive())
+                return;
+
+            var rect = new Texture2D(graphicsDevice, GameConstants.SNAKE_SIZE, GameConstants.SNAKE_SIZE);
+            var data = new Color[GameConstants.SNAKE_SIZE * GameConstants.SNAKE_SIZE];
+            for (int i = 0; i < data.Length; ++i) data[i] = Color.Black;
+            var center = 33;
+            // Draw the snake eye.
+            data[center] = Color.White;
+            data[center + 1] = Color.White;
+            data[center + 10] = Color.White;
+            data[center + 11] = Color.White;
+            rect.SetData(data);
+            var coor = new Vector2(Position.X, Position.Y);
+            spriteBatch.Draw(rect, coor, Color.White);
+
+            var font = Content.Load<SpriteFont>("Fonts/Arial24");
+            // Show Game Over.
+            var text = $"GAME OVER";
+            spriteBatch.DrawString(font, text, new Vector2(250, 250), Color.Pink);
+            }
+
+    }
+}
---
+++ GameStart.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SnakeGame.GameEntities;
+
+namespace SnakeGame
+{
+    public class GameStart : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Snake _theSnake;
+
+        public GameStart()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // Set initial resolution.
+            _graphics.PreferredBackBufferWidth = GameConstants.WINDOW_WIDTH;
+            _graphics.PreferredBackBufferHeight = GameConstants.WINDOW_HEIGHT;
+            _graphics.GraphicsProfile = GraphicsProfile.HiDef;
+
+            IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            _theSnake = new Snake();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        int elapsedTime = 0;
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (!_theSnake.IsAlive())
+                return;
+
+            // Check if we need to make update or not.
+            elapsedTime += gameTime.ElapsedGameTime.Milliseconds;
+            if (elapsedTime < 200)
+                return;
+
+            elapsedTime = 0;
+
+            _theSnake.Update(Keyboard.GetState());
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Start the sprite batch.
+            _spriteBatch.Begin();
+
+            var font = Content.Load<SpriteFont>("Fonts/Arial24");
+            if (!_theSnake.IsAlive())
+            {
+                // Show Game Over.
+                var text = $"GAME OVER";
+                _spriteBatch.DrawString(font, text, new Vector2(250, 250), Color.Pink);
+            }
+
+            _theSnake.Draw(_graphics.GraphicsDevice, _spriteBatch);
+            
+            // End the sprite batch.
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
